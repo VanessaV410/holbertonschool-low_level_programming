4fbaaca2ba94d52c061c3b2a70cda753d16dc2d2
VanessaV410 <vanessa.vallon974@gmail.com>
2025-02-20 20:58:42 +0100

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - check the code.
+ * @n: The character to print
+ *
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
