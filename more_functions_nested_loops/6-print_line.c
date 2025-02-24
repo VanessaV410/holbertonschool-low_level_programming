7c9acf44473fa1a4ce7326f723903e70e5109bef
VanessaV410 <vanessa.vallon974@gmail.com>
2025-02-24 14:59:31 +0100

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ *@n : the character to print
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		_putchar('_');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
