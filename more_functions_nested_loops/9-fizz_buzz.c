4852821e7125d03b53c42ce9c90cd49dd4e4e52d
VanessaV410 <vanessa.vallon974@gmail.com>
2025-02-25 15:47:56 +0100

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - check the code.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			putchar(' ');
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			putchar(' ');
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+			putchar(' ');
+		}
+	return (0);
+}
+
