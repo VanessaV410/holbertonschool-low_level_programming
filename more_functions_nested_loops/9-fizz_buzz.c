3de7140fee4dba16cbc03a9b0f9f7ff04859bdc6
VanessaV410 <vanessa.vallon974@gmail.com>
2025-02-25 19:21:54 +0100

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,26 +10,25 @@ int main(void)
 	int i;
 
 	for (i = 1 ; i <= 100 ; i++)
-
-		if (i % 3 == 0)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
-			putchar(' ');
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-			putchar(' ');
-		}
-		else if (i % 15 == 0)
-		{
-			printf("FizzBuzz");
 		}
 		else
 		{
 			printf("%d", i);
-			putchar(' ');
 		}
+		putchar(' ');
+	}
 	return (0);
 }
 
